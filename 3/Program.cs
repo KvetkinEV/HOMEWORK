d2ebebed3980cea628fdae44da1884636eb24dc4
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-06 02:22:41 +0300

    Add programm 3

---
+++ 3/Program.cs
@@ -0,0 +1,13 @@
+﻿double  a;
+Console.WriteLine("Введите число!");
+a = Convert.ToDouble(Console.ReadLine());
+if (a % 2 == 0)
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
+Console.ReadLine();
+
