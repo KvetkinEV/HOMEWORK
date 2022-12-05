caa0e60e58902da9c4ee16122f271843b75a03bb
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-06 00:59:54 +0300

    Add programm 1

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿double a, b;
+Console.WriteLine("Введите первое число!");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число!");
+b = Convert.ToDouble(Console.ReadLine());
+if (a > b)
+{
+Console.WriteLine("max = " + a);
+}
+else
+{
+Console.WriteLine("max = " + b);
+}
+Console.ReadLine();
