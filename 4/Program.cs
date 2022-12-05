bdb80ccf5cd8fb8598e204cc4462d56c272c07cd
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-06 02:25:56 +0300

    Add programm 4

---
+++ 4/Program.cs
@@ -0,0 +1,8 @@
+﻿double  N;
+Console.WriteLine("Введите число!");
+N = Convert.ToDouble(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+if (i % 2 == 0)
+Console.Write(i+ " ");
+Console.ReadLine();
+
